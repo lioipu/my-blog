96b307cd46d24fe4da7232292c9e0d6f1ae5caf4
lioipu <57569699+lioipu@users.noreply.github.com>
2025-04-02 17:14:25 +0300

    refactor: adds option of presenting post content to page (regular post, main post, home btn)

--- data.js
+++ data.js
@@ -4,12 +4,12 @@
 export const posts = [
     { 
       isMain: true,
-      image: "images/article-image-01.png",
+      image: "images/my-study-desk-iphone-copy.png",
       date: "MARCH 29, 2025",
       title: 'My new journey as a bootcamp student.',
       intro: `After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.`,
       content: {
-        titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
+        titles:   ["How I stay committed to learning<br><br>", "How I got started<br><br>"],
         contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
       
       },
@@ -22,8 +22,8 @@ export const posts = [
         title: 'Blog one',
         intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
         content: {
-          titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
-          contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+          titles:   ["How I stay committed to learning<br><br>", "How I got started<br><br>"],
+          contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.<br><br> While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.<br><br>`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.<br><br> That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
 
         },
         uuid: crypto.randomUUID()
@@ -32,7 +32,7 @@ export const posts = [
         isMain: false,
         image: "images/article-image-01.png",
         date: "JULY 23, 2022",
-        title: 'Blog one',
+        title: 'Blog two',
         intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
         content: {
           titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
@@ -87,7 +87,7 @@ export const posts = [
         title: 'Blog one',
         intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
         content: {
-          titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
+          titles:   ["How I stay committed to learning", "How I got started"],
           contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
 
         },
--- index.js
+++ index.js
@@ -1,22 +1,71 @@
 import {posts} from './data.js'
 
+const state = {
+    home: true,
+    about: false,
+    article: false
+}
+
 document.addEventListener('click', e => {
     let currentEventUuid = e.currentTarget.activeElement.dataset.postUuid
-    render(posts.find(post => post.uuid === currentEventUuid).uuid)
+    console.log(e.currentTarget.activeElement)
+    console.log(currentEventUuid)
+    if(currentEventUuid) {
+        state.article = true
+        render(posts.find(post => post.uuid === currentEventUuid))
+    } else if(e.target.className === 'link') {
+        if(e.target.dataset.home === 'home') {
+            state.home = true
+            render()
+        } else {
+            state.about = true
+            render()
+        }
+    }
 })
 
-function getMainPost(postUuid = posts.find( post => post.isMain === true).uuid) {
-    let mainPost = posts.find( post => post.uuid === postUuid)
-    let postStr = `<p id="date">${mainPost.date}</p>
-    <h2>${mainPost.title}</h2>
-    <p id="content">${mainPost.intro}</p>
-    `
+/* give us the main article that were on at the moment and present it on the screen */
+function getMain(currentPost = posts.find( post => post.isMain === true)) {
+    let postStr = ''
+    if(state.home) {
+        state.home = false
+        console.log('home state')
+        postStr = `<a href="#" id="main-post-container" data-post-uuid="${currentPost.uuid}" >
+            <article id="main-post">
+                <p id="date">${currentPost.date}</p>
+                <h2>${currentPost.title}</h2>
+                <p id="content">${currentPost.intro}</p>
+            </article>
+        </a>
+        `
+    } else if(state.article) {
+        console.log('article state')
+        state.article = false
+        postStr = `<div id="post-container">
+            <article>
+                <p id="date">${currentPost.date}</p>
+                <h2>${currentPost.title}</h2>
+                <p id="content">${currentPost.intro}</p>
+                <img class="post-image" src="${currentPost.image}">
+        `
+        for(let i = 0 ; i < currentPost.content.titles.length ; i++) {
+            postStr += `<h3>${currentPost.content.titles[i]}</h3>
+                <p>${currentPost.content.contents[i]}</p>
+                `
+            }
+            postStr += `
+            </article>
+        </div>`
+        } else {
+            // about page code to be implemented here
+        }
+    
     return postStr
 }
 
 function getPostsList() {
     let postsStr = posts.filter( post => post.isMain === false).map( post => {
-        return `<a class="blog" href="" data-post-uuid="${post.uuid}">
+        return `<a class="blog" href="#" data-post-uuid="${post.uuid}">
             <article>
                 <img class="post-image" src="${post.image}">
                 <header class="post">
@@ -34,9 +83,9 @@ function getPostsList() {
     return postsStr
 }
 
-function render(postUuid = posts.find( post => post.isMain === true).uuid) {
+function render(currentPost = posts.find( post => post.isMain === true)) {
     document.getElementById('posts').innerHTML = getPostsList() 
-    document.getElementById('main-post').innerHTML = getMainPost(postUuid)
+    document.getElementById('main').innerHTML = getMain(currentPost)
 
 }
 
