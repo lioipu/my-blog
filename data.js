93cb097a71e1e7d2e9ddcca1d7fcf0f27c5316f7
lioipu <57569699+lioipu@users.noreply.github.com>
2025-04-08 17:50:15 +0300

    feat: enable dynamic padding in main post text fix: fixing size defferences in images in grid, fixing posts appearience (present from new to old), while in spacipic article page he will Not appear in recent post

--- data.js
+++ data.js
@@ -2,20 +2,92 @@
 
 
 export const posts = [
-    { 
-      isMain: true,
-      image: "images/my-study-desk-iphone-copy.png",
-      date: "MARCH 29, 2025",
-      title: 'My new journey as a bootcamp student.',
-      intro: `After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.`,
-      content: {
-        titles:   ["How I stay committed to learning<br>", "How I got started<br>"],
-        contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+  { 
+    isMain: true,
+    isOn: false,
+    image: "images/my-study-desk-iphone-copy.png",
+    date: "MARCH 29, 2025",
+    title: 'There is many ways to reach the same goal.',
+    intro: `After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.`,
+    content: {
+      titles:   ["How I stay committed to learning<br>", "How I got started<br>"],
+      contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
       
+    },
+    uuid: crypto.randomUUID()
+  },
+  {
+      isOn: false,
+      isMain: false,
+      image: "images/blog-six.png",
+      date: "JULY 23, 2022",
+      title: 'Blog six',
+      intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+      content: {
+        titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
+        contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+  
       },
       uuid: crypto.randomUUID()
+  },
+  {
+      isOn: false,
+      isMain: false,
+      image: "images/blog-five.png",
+      date: "JULY 23, 2022",
+      title: 'Blog five',
+      intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+      content: {
+        titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
+        contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+
+      },
+      uuid: crypto.randomUUID()
+  }, 
+  {
+      isOn: false,
+      isMain: false,
+      image: "images/blog-four.png",
+      date: "JULY 23, 2022",
+      title: 'Blog four',
+      intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+      content: {
+        titles:   ["How I stay committed to learning<br>", "How I got started<br>"],
+        contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.<br> While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.<br>`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.<br><br> That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+
+      },
+      uuid: crypto.randomUUID()
+  }, 
+  {
+    isOn: false,
+    isMain: false,
+    image: "images/blog-three.png",
+    date: "JULY 23, 2022",
+    title: 'Blog three',
+    intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    content: {
+      titles:   ["How I stay committed to learning", "How I got started"],
+      contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+      
     },
+    uuid: crypto.randomUUID()
+  },
+  {
+    isOn: false,
+    isMain: false,
+    image: "images/blog-two.png",
+    date: "JULY 23, 2022",
+    title: 'Blog two',
+    intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    content: {
+      titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
+      contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
+      
+    },
+    uuid: crypto.randomUUID()
+  }, 
     {
+      isOn: false,
       isMain: false,
       image: "images/blog-one.png",
       date: "JULY 23, 2022",
@@ -27,71 +99,6 @@ export const posts = [
         
       },
       uuid: crypto.randomUUID()
-    }, 
-    {
-      isMain: false,
-      image: "images/blog-two.png",
-      date: "JULY 23, 2022",
-      title: 'Blog one',
-      intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
-      content: {
-        titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
-        contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
-        
-      },
-      uuid: crypto.randomUUID()
-    }, 
-    {
-      isMain: false,
-      image: "images/blog-three.png",
-      date: "JULY 23, 2022",
-      title: 'Blog one',
-      intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
-      content: {
-        titles:   ["How I stay committed to learning", "How I got started"],
-        contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
-        
-      },
-      uuid: crypto.randomUUID()
-    },
-    {
-        isMain: false,
-        image: "images/blog-four.png",
-        date: "JULY 23, 2022",
-        title: 'Blog one',
-        intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
-        content: {
-          titles:   ["How I stay committed to learning<br>", "How I got started<br>"],
-          contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.<br> While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.<br>`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.<br><br> That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
-
-        },
-        uuid: crypto.randomUUID()
-    }, 
-    {
-        isMain: false,
-        image: "images/blog-five.png",
-        date: "JULY 23, 2022",
-        title: 'Blog two',
-        intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
-        content: {
-          titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
-          contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
-
-        },
-        uuid: crypto.randomUUID()
-    }, 
-    {
-        isMain: false,
-        image: "images/blog-six.png",
-        date: "JULY 23, 2022",
-        title: 'Blog three',
-        intro: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
-        content: {
-          titles:   ["How I stay committed to learning\n\n", "How I got started\n\n"],
-          contents: [`I like to think of myself as a lifelong learner. I used to spend hours and hours learning, then try to create simple projects using what I learned or work new techniques into existing projects.\n\n While that was fun, I felt like it would be helpful to share what I was learning and most things about my journey with the world.`, `I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.\n\n That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.`]
-
-        },
-        uuid: crypto.randomUUID()
     }
 
 ]
--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 import {posts} from './data.js'
 
-const state = {
-    home: true,
-    about: false,
-    article: false
-}
+let state = 'HOME'
 
 document.addEventListener('click', e => {
     let currentEventUuid = e.currentTarget.activeElement.dataset.postUuid
     if(currentEventUuid) {
-        state.article = true
+        state = 'ARTICLE'
         render(3, posts.find(post => post.uuid === currentEventUuid))
     } else if(e.target.className === 'link') {
         if(e.target.dataset.home === 'home') {
-            state.home = true
+            state = 'HOME'
             render()
         } else {
-            console.log('here')
-            state.about = true
+            console.log('about')
+            state = 'ABOUT'
             render()
         }
     } else if(e.target.dataset.viewMoreBtn) {
@@ -28,15 +24,14 @@ document.addEventListener('click', e => {
 })
 
 function viewMoreBtnClickHandler() {
-    state.home = true
-    render(0)
+    state = 'HOME'
+    render(posts.length)
 }
 
 /* give us the main article that were on at the moment and present it on the screen */
 function getMain(currentPost = posts.find( post => post.isMain === true)) {
     let postStr = ''
-    if(state.home) {
-        state.home = false
+    if(state === 'HOME') {
         postStr = `<a href="#" id="main-post-container" data-post-uuid="${currentPost.uuid}" >
             <article id="main-post">
                 <p id="date">${currentPost.date}</p>
@@ -45,13 +40,13 @@ function getMain(currentPost = posts.find( post => post.isMain === true)) {
             </article>
         </a>
         `
-    } else if(state.article || state.about) {
+    } else if(state === 'ARTICLE' || state === 'ABOUT') {
         postStr += `
         <div id="post-container">
             <article>
         `
-        if(state.article) {
-            state.article = false
+        if(state === 'ARTICLE') {
+            currentPost.isOn = true
             postStr += `
                 <p id="date">${currentPost.date}</p>
                 <h2>${currentPost.title}</h2>
@@ -59,7 +54,6 @@ function getMain(currentPost = posts.find( post => post.isMain === true)) {
                 <img class="post-image" src="${currentPost.image}">`
                 
             } else {
-                state.about = false
                 postStr += `
                 <div id="about-img-container">
                     <img class="about-img" src="${'images/my-profile.png'}">
@@ -87,15 +81,15 @@ function getMain(currentPost = posts.find( post => post.isMain === true)) {
     return postStr
 }
 
-function getPostsList(size) {
+function getPostsList(size, currentPost) {
     let postsStr = `<div id="posts">`
-    if(state.article || state.about) {
+    if(state === 'ARTICLE' || state === 'ABOUT') {
         document.getElementById('recent-post-title').innerHTML = 'Recent posts'
     } else {
         document.getElementById('recent-post-title').innerHTML = ''
     }
-    console.log('size:' + size)
-    postsStr += posts.filter( post => post.isMain === false).map( post => {
+    postsStr += posts.filter( post => ( post.isMain === false ) && ( post.isOn === false ) )
+    .map( post => {
         return `<a class="blog" href="#" data-post-uuid="${post.uuid}">
             <article>
                 <img class="post-image" src="${post.image}">    
@@ -107,23 +101,26 @@ function getPostsList(size) {
             </article>
         </a>
         `
-    }).slice(size).join('')
+    }).slice(0, size).join('')
 
-    if(state.home) {
+    currentPost.isOn = false
+
+    if(state === 'HOME') {
+        console.log('home?')
         document.getElementById('btn-container').innerHTML = `
         <button id="btn" data-view-more-btn="view-more-btn" >View More</button>`
     } else {
         document.getElementById('btn-container').innerHTML = ''
     }
 
-    
+
 
     return postsStr
 }
 
 function render( size = 3, currentPost = posts.find( post => post.isMain === true)) {
-    document.getElementById('posts-container').innerHTML = getPostsList(size)
     document.getElementById('main').innerHTML = getMain(currentPost)
+    document.getElementById('posts-container').innerHTML = getPostsList(size, currentPost)
 }
 
 render()
